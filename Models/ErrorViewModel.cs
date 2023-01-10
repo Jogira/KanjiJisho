139e43b03057b157a19e7f29c254de73deaf6b28
Jonathan <Zweitonegoismus@gmail.com>
2023-01-10 14:48:33 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KanjiJisho.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
